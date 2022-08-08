04842f6ab3a1bfb12da6563a767be242155d633a
Jeffrey Cheung <jeffreycheung1995@gmail.com>
2022-08-08 14:38:19 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Jeffrey Cheung on 8/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
